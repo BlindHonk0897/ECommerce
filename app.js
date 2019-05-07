f34e85526ff32f7ac94538d204803c8da723a204
2ndyrGroupA <2ndyrGroupA@pn.local>
2019-05-07 22:04:12 +0800

    files modified

--- app.js
+++ app.js
@@ -7,8 +7,8 @@ const session = require('express-session');
 const bodyParser = require('body-parser');
 
 var MongoClient = require('mongodb').MongoClient;
-//const uri = "mongodb://172.16.32.40:27017/";
-const uri = "mongodb://172.16.8.29:27017/";
+const uri = "mongodb://172.16.32.40:27017/";
+//const uri = "mongodb://172.16.8.29:27017/";
 let client = null;
 
 app.use(express.static(__dirname+'/public'));
@@ -32,6 +32,7 @@ app.get('/',(req,res)=>{
     res.render(__dirname+'/public/views/login.ejs');
 });
 
+
 /// --------Admin Start ---------////
 app.get('/adminItems',(req,res)=>{
     client.db("ECommerce").collection('Items').find({}).toArray(function(err,doc){
@@ -52,7 +53,18 @@ app.get('/adminDashboard',(req,res)=>{
     res.render(__dirname+'/public/views/adminDashboard.ejs',{'data':doc});
   });
 });
+app.post('/update',(req,res)=>{
+    // client.db("ECommerce").collection('Users').insertOne({'Name':'Seller1 seller1','Username':'seller1','Password':'seller1','Type':'Seller','Status':'Pending'});
+    var id = req.body.id;
+    var status = req.body.status
+    client.db("ECommerce").collection('Users').updateOne({'__id':id},{$set:{'Status':status}});
+    console.log(id);
+    console.log(status);
+ });
+ 
+
 /// --------Admin End ---------////
+
 
 
 /// ----Seller start ---- //////
@@ -71,12 +83,35 @@ app.get('/sellerUpdateItems',function(req,res){
 
 //// -----Seller End ---- /////
 
+//// ---- Client Start ----//////
+
+app.get('/clientItems',function(req,res){
+    client.db("ECommerce").collection('Items').find({}).toArray(function(err,doc){
+        console.log(doc);
+        res.render(__dirname+'/public/views/clientBuyItem.ejs',{'data':doc});
+     });
+})
+
+app.get('/clientValidate',function(req,res){
+    res.render(__dirname+'/public/views/clientValidate.ejs')
+})
+app.post('/validateItem',function(req,res){
+    //console.log(req.body.id);
+    var obj = {};
+	console.log('body: ' + JSON.stringify(req.body));
+    res.redirect(__dirname+'/public/views/clientValidate.ejs')
+})
+
+////// -----Client End ----//////
+
+
+
 app.post('/login',(req,res)=>{
     console.log(req.body);
     var Username = req.body.Username;
     var Password = req.body.Password;
     //client.db("ECommerce").collection('Users').insertOne({'Name':'Admin admin','Username':'admin','Password':'admin','Type':'Admin','EmailAdd':'alingasadan@gmail.com'});
-    //client.db("ECommerce").collection('Users').insertOne({'Name':'Seller seller','Username':'seller','Password':'seller','Type':'Seller','Status':'Pending'});
+   // client.db("ECommerce").collection('Users').insertOne({'Name':'Sample seller','Username':'S','Password':'12345','Type':'Seller','Status':'Approved'});
     //client.db("ECommerce").collection('Users').insertOne({'Name':'Client client','Username':'client','Password':'client','Type':'Client'});
     client.db("ECommerce").collection('Users').findOne({'Username':Username,'Password':Password},(err,doc)=>{
         if(err){
@@ -92,6 +127,11 @@ app.post('/login',(req,res)=>{
             }else if(doc.Type === 'Seller' && doc.Status ==="Approved"){
                 res.render(__dirname+"/public/views/seller_Approve_Success.ejs"); 
                 //req.flash('error',"Your account not found!");
+            }else if(doc.Type === 'Client'){
+               client.db("ECommerce").collection('Items').find({}).toArray(function(err,doc){
+                console.log(doc);
+                res.render(__dirname+'/public/views/clientBuyItem.ejs',{'data':doc});
+                });
             }else{
                 res.redirect('/');
             }
@@ -100,6 +140,8 @@ app.post('/login',(req,res)=>{
       });
 });
 
+
+/// ----- Registration ---  /////
 app.post('/register',(req,res)=>{
     console.log(req.body);
     var errors = [];
@@ -108,7 +150,6 @@ app.post('/register',(req,res)=>{
     if(req.body.Password != req.body.ConfirmPassword){
         errors.push("Password not match!");
     }
-    console.log(errors.length);
     if(errors.length==0){
         if(req.body.Type === 'Seller'){
             status = 'Pending';
@@ -131,27 +172,29 @@ app.post('/register',(req,res)=>{
                'Type':req.body.Type
            }
         }
-        console.log("Wala error");
+       // console.log("Wala error");
         client.db("ECommerce").collection('Users').findOne({'Username':req.body.Username},(err,doc)=>{
-            if(doc!=null){errors.push("Username alreadyexist")}else{
-                client.db("ECommerce").collection('Users').insertOne(toRegister);
-            }
+            if(doc!=null){errors.push("Username alreadyexist")};
+            console.log("get Username");
         });
         client.db("ECommerce").collection('Users').findOne({'EmailAdd':req.body.EmailAdd},(err,doc)=>{
-              if(doc!=null){errors.push("Email already exist")}else{
-                  client.db("ECommerce").collection('Users').deleteOne(toRegister);
-                  client.db("ECommerce").collection('Users').insertOne(toRegister);
-               }
-        });
+            if(doc!=null){errors.push("Email already exist")};
+            if(errors.length==0){   
+              client.db("ECommerce").collection('Users').insertOne(toRegister);
+            }else{
+                console.log('Wala na save');
+            }  
+            console.log("get Email"); 
+     });
+        
         //client.db("ECommerce").collection('Users').insertOne(toRegister);
     }
-    
+    res.redirect("/");
     // //console.log(toRegister);
     // for(i = 0;i<errors.length;i++){
     //     console.log(errors[i]);
     // }
     //client.db("ECommerce").collection('Users').insertOne({'Name':'Client client','Username':'client','Password':'client','Type':'Client'});
-    res.redirect("/");
 });
 const port = process.env.PORT || 3000;
 
